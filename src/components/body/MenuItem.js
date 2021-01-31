07f0f933367f6f0309563a0d63043966d03d8d08
Kamrul Hasan <kamrul.dev@gmail.com>
2021-01-31 12:53:05 +0600

    Disigning Menu Iteam

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardImgOverlay } from 'reactstrap';
 
 
 const MenuItem = (props) => {
-    console.log(props)
+    // console.log(props)
     return (
         <div>
-
+            <Card style={{ margin: "10px" }}>
+                <CardImg top width="100%" src={props.dish.image} alt={props.dish.name} style={{ opacity: "0.5" }} />
+                <CardBody>
+                    <CardImgOverlay>
+                        <CardTitle tag="h5">{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     );
 }
